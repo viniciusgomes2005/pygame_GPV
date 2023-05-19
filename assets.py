09aecf7ccd9d350dd7eebd28cc85b2933094b13e
GABRIEL NAKANO FUJIHARA <gabrielnf@al.insper.edu.br>
2023-05-19 12:10:44 -0300

    Primeiro Commite

--- assets.py
+++ assets.py
@@ -1,22 +1,17 @@
 import pygame
 import os
 from config import Player_largura, Player_altura, Zumbi_largura, Zumbie_altura, IMG_DIR, SND_DIR, FNT_DIR
+Normal_anim='animação player normal'
+
+
 def load_assets():
     assets = {}
-    
-    for i in range(9):
+    Normal_Anim=[]
+    for i in range(10):
         # Os arquivos de animação são numerados de 00 a 08
-        filename = os.path.join(IMG_DIR, 'regularExplosion0{}.png'.format(i))
+        filename = os.path.join(IMG_DIR, 'Player_Normal{}.png'.format(i))
         img = pygame.image.load(filename).convert()
-        img = pygame.transform.scale(img, (32, 32))
-        explosion_anim.append(img)
-    assets[EXPLOSION_ANIM] = explosion_anim
-    assets[SCORE_FONT] = pygame.font.Font(os.path.join(FNT_DIR, 'PressStart2P.ttf'), 28)
-
-    # Carrega os sons do jogo
-    pygame.mixer.music.load(os.path.join(SND_DIR, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
-    pygame.mixer.music.set_volume(0.4)
-    assets[BOOM_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl3.wav'))
-    assets[DESTROY_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl6.wav'))
-    assets[PEW_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'pew.wav'))
+        img = pygame.transform.scale(img, (Player_largura, Player_altura))
+        Normal_Anim.append(img)
+    assets[Normal_anim] = Normal_Anim
     return assets

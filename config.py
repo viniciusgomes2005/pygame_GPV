b69842d4386a09a52993f2417af3cafa1fc85373
GABRIEL NAKANO FUJIHARA <gabrielnf@al.insper.edu.br>
2023-05-19 11:07:38 -0300

    atualização

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 from os import path
 import pygame
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
+
+#Dados gerais do jogo
 Altura_janela = 800
 Largura_janela = 600
-window = pygame.display.set_mode((Altura_janela, Largura_janela))
-character_width=45
-character_height=55
+FPS=40
+#Tamanhos
+Player_largura=45
+Player_altura=55
+Zumbi_largura=45
+Zumbie_altura=55
+
 INIT=0
 GAME=1
 QUIT=2
